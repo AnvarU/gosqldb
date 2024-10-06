ae0a9743410793bb8194e0873cf0980d53419113
Anvar Umuraliev <aumuraliev71@gmail.com>
2024-10-06 19:35:47 +0200

    remove unused code and small cleanup

--- btree/btree.go
+++ btree/btree.go
@@ -44,76 +44,7 @@ func (n *Node) splitChild(i int) {
 	n.Children[i] = rightNode
 }
 
-func (n *Node) maybeSplit(b *BTree) (*Node, bool) {
-
-	if len(n.Keys) < b.GetMaxChild() {
-		return n, false
-	}
-
-	leftNode := &Node{
-		Keys:       make([]int, len(n.Keys)/2),
-		Children:   make([]*Node, 0, len(n.Keys)/2+1),
-		parentNode: n.parentNode,
-	}
-	copy(leftNode.Keys, n.Keys[:len(n.Keys)/2])
-
-	rightNode := &Node{
-		Keys:       make([]int, len(n.Keys)/2),
-		Children:   make([]*Node, 0, len(n.Keys)/2+1),
-		parentNode: n.parentNode,
-	}
-	copy(rightNode.Keys, n.Keys[len(n.Keys)/2+1:])
-
-	if len(n.Children) > 0 {
-		for i := 0; i < len(n.Children); i++ {
-			if i < len(n.Children)/2 {
-				leftNode.Children = append(leftNode.Children, n.Children[i])
-				n.Children[i].parentNode = leftNode
-			} else {
-				rightNode.Children = append(rightNode.Children, n.Children[i])
-				n.Children[i].parentNode = rightNode
-			}
-		}
-	}
-
-	leftoverKey := n.Keys[len(n.Keys)/2]
-
-	if n.parentNode != nil {
-
-		index := sort.Search(len(n.parentNode.Keys), func(i int) bool {
-			return n.parentNode.Keys[i] > leftoverKey
-		})
-		n.parentNode.Keys = append(n.parentNode.Keys, 0)
-		copy(n.parentNode.Keys[index+1:], n.parentNode.Keys[index:])
-		n.parentNode.Keys[index] = leftoverKey
-
-		oldChildren := n.parentNode.Children
-		n.parentNode.Children = make([]*Node, len(n.parentNode.Children)+1)
-		i := 0
-		for ; i < index; i++ {
-			n.parentNode.Children[i] = oldChildren[i]
-		}
-		n.parentNode.Children[i] = leftNode
-		i++
-		n.parentNode.Children[i] = rightNode
-		i++
-		for ; i < len(n.parentNode.Children); i++ {
-			n.parentNode.Children[i] = oldChildren[i-1]
-		}
-
-		return n.parentNode.maybeSplit(b)
-	} else {
-		newNode := &Node{
-			Keys:     []int{leftoverKey},
-			Children: []*Node{leftNode, rightNode},
-		}
-		leftNode.parentNode = newNode
-		rightNode.parentNode = newNode
-		return newNode, true
-	}
-}
-
-func (n *Node) Insert(b *BTree, k int) *Node {
+func (n *Node) insert(b *BTree, k int) *Node {
 	if n == nil {
 		return &Node{
 			Keys:     []int{k},
@@ -138,13 +69,8 @@ func (n *Node) Insert(b *BTree, k int) *Node {
 			}
 			childNode = n.Children[index]
 		}
-		childNode.Insert(b, k)
+		childNode.insert(b, k)
 	}
-
-	//newNode, splitted := n.maybeSplit(b)
-	//if splitted {
-	//	return newNode
-	//}
 
 	return n
 }
@@ -170,9 +96,9 @@ func (b *BTree) Insert(k int) {
 		newRoot.Children = append(newRoot.Children, b.Root)
 		b.Root = newRoot
 		newRoot.splitChild(0)
-		newRoot.Insert(b, k)
+		newRoot.insert(b, k)
 	} else {
-		b.Root.Insert(b, k)
+		b.Root.insert(b, k)
 	}
 }
 
